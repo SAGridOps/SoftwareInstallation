38281ef0f82b61f138d66a03161299d17844e0c7
Bruce Becker <bbecker@csir.co.za>
2014-06-20 11:01:30 +0200

    Update build.sh

--- math/freefem++/generic/build.sh
+++ math/freefem++/generic/build.sh
@@ -2,26 +2,42 @@
 # The FreeFEM++ build script
 # 
 # FreeFEM has the possibility to download all the dependencies
-module add ci
 
+# Add the module for the build environment
+# this will set up paths for the application artifact (build.tar.gz) as well as the directory in which to 
+# cache the source file.
+module add ci
+# the $SRC_DIR may not be present in the first run, so we force it here. 
+mkdir -vp $SRC_DIR
 SOURCE_FILE=$NAME-$VERSION.tar.gz
+# Check if the source file is available, if not, get it.
 if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
-	mkdir -vp $SRC_DIR
 	echo "getting the file"
 	wget http://www.freefem.org/ff++/ftp/$SOURCE_FILE  -O $SRC_DIR/$SOURCE_FILE
 else
-      	tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+      	echo "$SRC_FILE is already here"
 fi
+# We untar it into the working directory of the job.
 
+tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 ls
 cd freefem++-$VERSION
 ./configure --enable-download
-time nice -n20 make -j2 install DESTDIR=$WORKSPACE/build
+download/getall -a 
+# we don't want to have paralellism, because independent threads will die sometimes
+time make -j4
+time make -j4
+# At this point, the compiliation should either pass or fail. If the compilation passes, we should continue, else exit the script.
+# The application is installed in $WORKSPACE/build
+time nice -n20 make install DESTDIR=$WORKSPACE/build
 
 mkdir -p $REPO_DIR
 rm -rf $REPO_DIR/* 
 tar -cvzf $REPO_DIR/build.tar.gz -C $WORKSPACE/build apprepo
 
+# we need to make a few modules for the application :
+# 1. the ci build module
+# 2. the module that will be used by CVMFS clients
 mkdir -p modules
 (
 cat <<MODULE_FILE
@@ -36,10 +52,13 @@ proc ModulesHelp { } {
 module-whatis   "$NAME $VERSION."
 setenv       FREEFEM_VERSION       $VERSION
 setenv       FREEFEM_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+setenv       FREEFEM_DIR       
 prepend-path LD_LIBRARY_PATH   $::env(FREEFEM_DIR)/lib
 prepend-path PATH              $::env(FREEFEM_DIR)/bin
 MODULE_FILE
 ) > modules/$VERSION 
-# we need a new modules collection - Astro.
-mkdir -p $LIBRARIES_MODULES/$NAME 
+# w
+mkdir -p $LIBRARIES_MODULES/$NAME
 cp modules/$VERSION $LIBRARIES_MODULES/$NAME 
+
+# We need to make a CVMFS modulefile as well
