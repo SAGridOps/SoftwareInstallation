98539416ba13a397d7bc3588c9debb2a9898e954
Bruce Becker <bbecker@csir.co.za>
2014-06-20 14:51:04 +0200

    Update build.sh

--- Compilers/gmp/generic/build.sh
+++ Compilers/gmp/generic/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
 SOURCE_FILE=$NAME-$VERSION.tar.gz
+
+
+module load ci
+
 mkdir -p $WORKSPACE
 mkdir -p $SRC_DIR
 mkdir -p $SOFT_DIR
 
-module load ci
 
 if [[ ! -e $SRC_DIR/$SOURCE_FILE ]]
 then
