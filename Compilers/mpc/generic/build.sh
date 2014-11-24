4a04f9cccde8a185223ac948bf7db1cc21293c08
Bruce Becker <brucellino@gmail.com>
2014-11-24 16:10:16 +0100

    modified the build script for MPC

--- Compilers/mpc/generic/build.sh
+++ Compilers/mpc/generic/build.sh
@@ -1,20 +1,38 @@
 #!/bin/bash -e
+###############################################
+# Africa-Arabia Regional Operations Centre
+# This source code is under the copyright of CSIR Meraka
+# Title                   : build.sh
+# First Author            : Bruce Becker
+# Institute               : CSIR Meraka Institute
+###############################################
+#
+# This is the build script for the boost C++ libraries.
+# See http://www.boost.org/
+#
 SOURCE_FILE=$NAME-$VERSION.tar.gz
 
 module load ci
-module load gmp/5.1.3
-module load mpfr/3.1.2
+# Load other modules here if you need to.
+module load gcc
 
-rm -rf $GMP_DIR
-tar -xvzf /repo/$SITE/$OS/$ARCH/gmp/$GMP_VERSION/build.tar.gz -C /
-
+# This will go away when we have NFS
+rm -rf $GCC_DIR
+# Pull in the build artifact for GCC
+tar -xvzf /repo/$SITE/$OS/$ARCH/gcc/$GCC_VERSION/build.tar.gz -C /
+# And the dependencies
 rm -rf $MPFR_DIR
 tar -xvzf /repo/$SITE/$OS/$ARCH/mpfr/$MPFR_VERSION/build.tar.gz -C /
+rm -rf $MPC_DIR
+tar xvfz /repo/$SITE/$OS/$ARCH/mpc/$MPC_VERSION/build.tar.gz -C /
 
+###############################################################################
+# Get the source code if we don't already have it
+###############################################################################
 if [[ ! -e $SRC_DIR/$SOURCE_FILE ]]
 then
         mkdir -p $SRC_DIR
-	wget http://mirror.ufs.ac.za/gnu/gnu/mpc/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+	wget http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.gz/download -O boost-1.55.0.tar.gz  -O $SRC_DIR/$SOURCE_FILE
 fi
 tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 
